a22d6ceeb8a151da8d7bf68888cbe3ce99e6ec4c
엄지성( ) / DX팀(eom1986@xisnd.com) <eom1986@xisnd.com>
2026-03-25 13:28:17 +0900

    GitHub Pages 배포를 위한 정적 내보내기 및 액션 워크플로우 추가

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
+  basePath: '/dx-team-lab-xisnd-minwon-talk-app',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -29,6 +26,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
